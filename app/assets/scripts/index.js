3ad618051a9878ec9bc1629d7625ed54782910b7
Rami Ashqar <ramiashqar@MacBook-Pro.local>
2019-03-28 23:33:05 +0300

    prepared about section

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -3,3 +3,16 @@ require("../styles/app.scss");
 require("../../index.html");
 require('./styles');
 
+//TODO:
+
+//[1] Fix the nav to the top and reduce the height when scrolling
+//[2] Hide the down arrow when scrolling matching Apple design
+//[]
+//[]
+
+//FIXME:
+
+//[]
+//[]
+//[]
+//[]
