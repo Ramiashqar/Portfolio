352731b5c9cc6112927dff026dda9232f7d6aa9e
Rami Ashqar <ramiashqar@MacBook-Pro.local>
2019-03-30 02:37:20 +0300

    footer is ready and bug fixes

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -7,8 +7,8 @@ require('./styles');
 
 //[1] Fix the nav to the top and reduce the height when scrolling
 //[2] Hide the down arrow when scrolling matching Apple design
-//[]
-//[]
+//[3] Hover Effects to: big logo and face image
+//[4] Add id's to sections and link the nav items and "contact" in skills too.
 
 //FIXME:
 
